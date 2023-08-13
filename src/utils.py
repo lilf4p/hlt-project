716d2eeb91164721cce9b704dd2b05bb2625c4a1
Leonardo Stoppani <58340900+lilf4p@users.noreply.github.com>
2023-08-13 11:08:22 +0200

    change load utils

--- src/utils.py
+++ src/utils.py
@@ -2,7 +2,20 @@ import json
 import os
 import pandas as pd
 
-def load_ECHR(anon:bool=False):
+def load_ECHR(path:str, anon:bool=False):
+
+    """Load ECHR dataset from json to pandas dataframe
+    
+    Args:
+        path (str): path to ECHR dataset
+        anon (bool, optional): if True, load anonymized dataset. Defaults to False.
+        
+        Returns:
+            df_train (pd.DataFrame): train dataset
+            df_dev (pd.DataFrame): dev dataset
+            df_test (pd.DataFrame): test dataset
+            
+    """
 
     # load train, dev and test dataset from json to pandas dataframe
     sfx = '_Anon' if anon else ''
@@ -13,7 +26,7 @@ def load_ECHR(anon:bool=False):
     df_test = pd.DataFrame()
 
     # train dataset
-    train_path = 'ECHR_Dataset/EN_train'+sfx+'/'
+    train_path = path+'/EN_train'+sfx+'/'
     # for all the files in a directory 
     for filename in os.listdir(train_path):
         with open(train_path+filename) as f:
@@ -23,7 +36,7 @@ def load_ECHR(anon:bool=False):
             df_train = df_train._append(df, ignore_index=True)
 
     # dev dataset
-    dev_path = 'ECHR_Dataset/EN_dev'+sfx+'/'
+    dev_path = path+'/EN_dev'+sfx+'/'
     # for all the files in a directory
     for filename in os.listdir(dev_path):
         with open(dev_path+filename) as f:
@@ -33,7 +46,7 @@ def load_ECHR(anon:bool=False):
             df_dev = df_dev._append(df, ignore_index=True)
     
     # test dataset
-    test_path = 'ECHR_Dataset/EN_test'+sfx+'/'
+    test_path = path+'/EN_test'+sfx+'/'
     # for all the files in a directory
     for filename in os.listdir(test_path):
         with open(test_path+filename) as f:
@@ -45,7 +58,21 @@ def load_ECHR(anon:bool=False):
     # return train, dev and test dataset
     return df_train, df_dev, df_test
 
-def load_ECHR_small(anon:bool=False, n:int=100):
+def load_ECHR_small(path:str, anon:bool=False, n:int=100):
+
+    """Load ECHR dataset from json to pandas dataframe
+
+    Args:
+        path (str): path to ECHR dataset
+        anon (bool, optional): if True, load anonymized dataset. Defaults to False.
+        n (int, optional): number of files to load. Defaults to 100.
+    
+    Returns:
+        df_train (pd.DataFrame): train dataset
+        df_dev (pd.DataFrame): dev dataset
+        df_test (pd.DataFrame): test dataset
+
+    """
 
     # load train, dev and test dataset from json to pandas dataframe
     sfx = '_Anon' if anon else ''
@@ -58,7 +85,7 @@ def load_ECHR_small(anon:bool=False, n:int=100):
     i = 0
 
     # train dataset
-    train_path = 'ECHR_Dataset/EN_train'+sfx+'/'
+    train_path = path+'/EN_train'+sfx+'/'
     # for all the files in a directory 
     for filename in os.listdir(train_path):
 
@@ -76,7 +103,7 @@ def load_ECHR_small(anon:bool=False, n:int=100):
     i = 0
 
     # dev dataset
-    dev_path = 'ECHR_Dataset/EN_dev'+sfx+'/'
+    dev_path = path+'/EN_dev'+sfx+'/'
     # for all the files in a directory
     for filename in os.listdir(dev_path):
 
@@ -94,7 +121,7 @@ def load_ECHR_small(anon:bool=False, n:int=100):
     i = 0
 
     # test dataset
-    test_path = 'ECHR_Dataset/EN_test'+sfx+'/'
+    test_path = path+'/EN_test'+sfx+'/'
     # for all the files in a directory
     for filename in os.listdir(test_path):
 
@@ -114,13 +141,13 @@ def load_ECHR_small(anon:bool=False, n:int=100):
 
 if __name__ == "__main__":
     # test load_ECHR
-    df_train, df_dev, df_test = load_ECHR()
+    df_train, df_dev, df_test = load_ECHR('ECHR_Dataset')
     print(df_train)
     print(df_dev)
     print(df_test)
 
     # test load_ECHR_small
-    df_train, df_dev, df_test = load_ECHR_small(n=10)
+    df_train, df_dev, df_test = load_ECHR_small('ECHR_Dataset',n=10)
     print(df_train)
     print(df_dev)
     print(df_test)
