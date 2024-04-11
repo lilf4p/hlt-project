d66ef0b4d87379b04a8dc6c0836091be9f27f288
lilf4p <l.stoppani@studenti.unipi.it>
2024-04-11 20:26:12 +0200

    minor

--- notebook/hierbert.py
+++ notebook/hierbert.py
@@ -17,34 +17,38 @@ import sys
 sys.path.append('..')
 from src.attentionmlp import AttentionMLP
 
-# load tokenized data
-path_train ='/storagenfs/l.stoppani/hlt-project/hlt-project/ECHR_Dataset/train.csv'
-path_dev ='/storagenfs/l.stoppani/hlt-project/hlt-project/ECHR_Dataset/dev.csv'
-path_test ='/storagenfs/l.stoppani/hlt-project/hlt-project/ECHR_Dataset/test.csv'
+tokenize_data = True
 
-df_train = pd.read_csv(path_train)
-df_dev = pd.read_csv(path_dev)
-df_test = pd.read_csv(path_test)
+if tokenize_data:
 
-tokenizer = AutoTokenizer.from_pretrained("distilbert/distilbert-base-uncased")
-def preprocess_function(examples):
-    return tokenizer(examples["text"], padding=False)
+    # load  data
+    path_train ='/storagenfs/l.stoppani/hlt-project/hlt-project/ECHR_Dataset/train.csv'
+    path_dev ='/storagenfs/l.stoppani/hlt-project/hlt-project/ECHR_Dataset/dev.csv'
+    path_test ='/storagenfs/l.stoppani/hlt-project/hlt-project/ECHR_Dataset/test.csv'
 
-from datasets import Dataset
-df_train = Dataset.from_pandas(df_train)
-df_dev = Dataset.from_pandas(df_dev)
-df_test = Dataset.from_pandas(df_test)
+    df_train = pd.read_csv(path_train)
+    df_dev = pd.read_csv(path_dev)
+    df_test = pd.read_csv(path_test)
 
-df_train = df_train.map(preprocess_function, batched= True)
-df_dev = df_dev.map(preprocess_function, batched= True)
-df_test = df_test.map(preprocess_function, batched= True)
+    tokenizer = AutoTokenizer.from_pretrained("distilbert/distilbert-base-uncased")
+    def preprocess_function(examples):
+        return tokenizer(examples["text"], padding=False)
 
-df_train = df_train.with_format(type = "pandas", columns= ['input_ids', 'attention_mask', 'label'] )
-df_dev = df_dev.with_format(type = "pandas", columns= ['input_ids', 'attention_mask', 'label'])
-df_test = df_test.with_format(type = "pandas", columns= ['input_ids', 'attention_mask', 'label'])
+    from datasets import Dataset
+    df_train = Dataset.from_pandas(df_train)
+    df_dev = Dataset.from_pandas(df_dev)
+    df_test = Dataset.from_pandas(df_test)
 
-#documents = df_train[['input_ids', 'attention_mask', 'label']]
-# convert the series into a list
+    df_train = df_train.map(preprocess_function, batched= True)
+    df_dev = df_dev.map(preprocess_function, batched= True)
+    df_test = df_test.map(preprocess_function, batched= True)
+
+    df_train = df_train.with_format(type = "pandas", columns= ['input_ids', 'attention_mask', 'label'] )
+    df_dev = df_dev.with_format(type = "pandas", columns= ['input_ids', 'attention_mask', 'label'])
+    df_test = df_test.with_format(type = "pandas", columns= ['input_ids', 'attention_mask', 'label'])
+
+    # save the tokenized data to csv files
+
 input_ids = df_train['input_ids'].tolist()
 attention_mask = df_train['attention_mask'].tolist()
 labels = df_train['label'].tolist()
@@ -146,7 +150,7 @@ class Hierbert(nn.Module):
             )
 
         bert_output = torch.stack(bert_output)
-        print(bert_output.shape)
+        #print(bert_output.shape)
         bert_output[torch.isnan(bert_output)] = 0
         sentence_mask = make_mask(input_ids, lengths).to('cuda')
 
@@ -156,8 +160,8 @@ dataset = ECHRDataset(input_ids, attention_mask, labels)
 val_dataset = ECHRDataset(val_input_ids, val_attention_mask, val_labels)
 
 # create a dataloader
-dataloader = DataLoader(dataset, batch_size=4, shuffle=True, collate_fn=collate_fn)
-val_dataloader = DataLoader(val_dataset, batch_size=4, shuffle=True, collate_fn=collate_fn)
+dataloader = DataLoader(dataset, batch_size=1, shuffle=True, collate_fn=collate_fn)
+val_dataloader = DataLoader(val_dataset, batch_size=1, shuffle=True, collate_fn=collate_fn)
 
 #bert = BertModel.from_pretrained('nlpaueb/legal-bert-base-uncased')
 bert = AutoModel.from_pretrained("distilbert/distilbert-base-uncased") # distilbert-base-uncased instead of bert-base-uncased for memory issues
@@ -165,7 +169,7 @@ bert = AutoModel.from_pretrained("distilbert/distilbert-base-uncased") # distilb
 accelerator = Accelerator(gradient_accumulation_steps=2)
 
 model = Hierbert(bert=bert, hidden_sizes=[768, 16])
-optimizer = Adam(model.parameters(), lr=0.001, weight_decay=0.0001)
+optimizer = Adam(model.parameters(), lr=0.001)
 
 loss_function = torch.nn.BCELoss()
 
@@ -229,3 +233,45 @@ for epoch in range(4):
         best_model = model.state_dict()
 
 torch.save(best_model, 'hierbert.pt')
+
+# test the model
+test_input_ids = df_test['input_ids'].tolist()
+test_attention_mask = df_test['attention_mask'].tolist()
+test_labels = df_test['label'].tolist()
+
+test_input_ids = [torch.stack(split_into_chunks(torch.tensor(i))) for i in test_input_ids]
+test_attention_mask = [torch.stack(split_into_chunks(torch.tensor(i))) for i in test_attention_mask]
+test_input_ids =[torch.squeeze(i, dim=1) for i in test_input_ids]
+test_attention_mask =[torch.squeeze(i, dim=1) for i in test_attention_mask]
+test_lengths =[i.size(0) for i in test_input_ids]
+
+test_dataset = ECHRDataset(test_input_ids, test_attention_mask, test_labels)
+test_dataloader = DataLoader(test_dataset, batch_size=2, shuffle=True, collate_fn=collate_fn)
+
+# load the best model
+model.load_state_dict(torch.load('hierbert.pt'))
+
+model.eval()
+test_loss = 0
+test_accuracy = 0
+with torch.no_grad():
+    for batch in tqdm(test_dataloader, desc=f"Test"):
+        inputs, att, targets, l = batch
+        outputs = model(inputs, att, l)
+        loss = loss_function(outputs, targets.float())
+        test_loss += loss.item()
+        outputs = torch.round(outputs)
+        accuracy = f1_score(targets.cpu().numpy(), outputs.cpu().numpy(), average='weighted')
+        test_accuracy += accuracy
+        del inputs
+        del att
+        del targets
+        del l
+        del outputs
+        del loss
+        torch.cuda.empty_cache()
+
+test_loss = test_loss / len(test_dataloader)
+test_accuracy = test_accuracy / len(test_dataloader)
+print(f"Test Loss: {test_loss} - Test F1: {test_accuracy}")
+
